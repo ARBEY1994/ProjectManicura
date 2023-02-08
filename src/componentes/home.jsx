37fce7747c64e9ed581f4ab35bf47c09fb970d6f
liamgabriel2021 <arbey221@gmail.com>
2023-02-07 23:31:38 -0500

    logo

--- src/componentes/home.jsx
+++ src/componentes/home.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+
 import NavBar from "./navBar";
 import Carousel from "./Carrusel";
-import "../carousel/carousel.css";
-import "../carousel/carousel.rtl.css";
+
 import About from "./About";
 import Footer from "./footer";
-import Placeholder from "react-bootstrap/Placeholder";
 
 const Home = () => {
   return (
--- src/componentes/navBar.jsx
+++ src/componentes/navBar.jsx
@@ -2,58 +2,53 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 
+import logo from "../Fotos/AnnyLogo2.jpg";
+
 const NavBar = () => {
   return (
-    <header>
-      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            Anny Julieth Nail Art
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarCollapse"
-            aria-controls="navbarCollapse"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-        </div>
-
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav me-auto mb-2 mb-md-0">
-            <li class="nav-item">
-              <a
-                class="nav-link active"
-                aria-current="page"
-                href="#segundaFoto"
-              >
-                <ButtonGroup aria-label="Basic example">
-                  <Button variant="secondary">Semipermanentes</Button>
-                </ButtonGroup>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#tercerFoto">
-                <ButtonGroup aria-label="Basic example">
-                  <Button variant="secondary">Tradicionales</Button>
-                </ButtonGroup>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#primerFoto">
-                <ButtonGroup aria-label="Basic example">
-                  <Button variant="secondary">Acrilicas</Button>
-                </ButtonGroup>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </header>
+    <nav class="navbar navbar-expand-md navbar-white fixed-top bg-white">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src={logo} alt="no found" />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarCollapse"
+          aria-controls="navbarCollapse"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#segundaFoto">
+              <ButtonGroup aria-label="Basic example">
+                <Button variant="secondary">Semipermanentes</Button>
+              </ButtonGroup>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#tercerFoto">
+              <ButtonGroup aria-label="Basic example">
+                <Button variant="secondary">Tradicionales</Button>
+              </ButtonGroup>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#primerFoto">
+              <ButtonGroup aria-label="Basic example">
+                <Button variant="secondary">Acrilicas</Button>
+              </ButtonGroup>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
