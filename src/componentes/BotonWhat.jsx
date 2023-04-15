dec6ab068316a714e21f4972d1d0e1efdf3d0fbc
liamgabriel2021 <arbey221@gmail.com>
2023-04-15 10:39:30 -0500

    cambios en logo whats

--- src/componentes/BotonWhat.jsx
+++ src/componentes/BotonWhat.jsx
@@ -2,34 +2,15 @@ import React from "react";
 import "../Styles/whatsapp.css";
 
 const BotonWhat = () => {
-  // const style = {
-  //   position: "fixed",
-  //   bottom: "20px",
-  //   right: "30px",
-  //   width: "55px",
-  //   height: "55px",
-  //   borderRadius: "50%",
-  //   backgroundColor: "green",
-  //   display: "flex",
-  //   justifyContent: "center",
-  //   alignItems: "center",
-
-  // };
-
-  // const iconStyle = {
-  //   color: "white",
-  //   fontSize: "50px",
-
-  // };
-
   return (
-    <div className="style">
+    <div className="styleWhat">
       <a
         href="https://wa.me/573133299109?text=hola,Anny,¿tienes agenda para hoy?"
         target="_blank"
         rel="noreferrer"
       >
         <i className="fab fa-whatsapp iconStyle"></i>
+        <span class="message-porLeer">1</span>
       </a>
     </div>
   );
