9dc000bd0b5856afbc8d6fe55ea9bf580674b98d
liamgabriel2021 <arbey221@gmail.com>
2023-04-14 22:20:53 -0500

    new

--- src/componentes/Fotografias.jsx
+++ src/componentes/Fotografias.jsx
@@ -30,7 +30,7 @@ const Fotografia = () => {
       <NavBarDetail />
 
       <div class="container px-4 py-5" id="custom-cards">
-        <h2 class="pb-2 border-bottom">Mis trabajos...</h2>
+        <h2 class="pb-2 border-bottom text-muted ">Mis trabajos...</h2>
 
         <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
           <div class="col">
